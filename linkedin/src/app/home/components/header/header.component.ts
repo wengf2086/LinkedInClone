99c00591f709ce4f86535b568fa8a208097f1bea
wengf2086 <wengf2086@gmail.com>
2023-07-24 18:32:01 -0400

    Created header with icons, search bar, border, and profile. Now working on Profile Dropdown

--- linkedin/src/app/home/components/header/header.component.ts
+++ linkedin/src/app/home/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+import { PopoverComponent } from './popover/popover.component';
 
 @Component({
   selector: 'app-header',
@@ -7,8 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent  implements OnInit {
 
-  constructor() { }
+  constructor(public popoverController: PopoverController) { }
 
   ngOnInit() {}
+
+  async presentPopover(e: Event) {
+    const popover = await this.popoverController.create({
+      component: PopoverComponent,
+      cssClass: 'my-custom-class',
+      event: e,
+      showBackdrop: false,
+    });
+
+    await popover.present();
+
+    const { role } = await popover.onDidDismiss();
+    console.log(`Popover dismissed with role: ${role}`);
+  }
 
 }
